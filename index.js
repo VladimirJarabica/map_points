2e2717060da22057d9f064cbf9ec7f036a40375d
VladimirJarabica <vladojarabica@gmail.com>
2017-02-09 21:14:58 +0100

    module exports

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 require("babel-register")
-const Immutable = require("immutable")
-
-const installDevTools = require("immutable-devtools")
-installDevTools(Immutable)
-
-import init from "./src/main"
-
-window.init = init
-
-export default init
+module.exports = require("./main.js")
---
+++ main.js
@@ -0,0 +1,10 @@
+const Immutable = require("immutable")
+
+const installDevTools = require("immutable-devtools")
+installDevTools(Immutable)
+
+import init from "./src/main"
+
+window.init = init
+
+export default init
