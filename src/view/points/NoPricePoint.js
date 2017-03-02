7bcf2bcca8ca0dce187df6426cc57d4925aa7acf
VladimirJarabica <vladojarabica@gmail.com>
2017-03-02 16:45:08 +0100

    add styles

--- src/view/points/NoPricePoint.js
+++ src/view/points/NoPricePoint.js
@@ -2,10 +2,40 @@ import Point from "./Point"
 
 export default class NoPricePoint extends Point {
 	constructor(map, point) {
-		const el = document.createElement("div")
-		super(el, point)
-		el.className += " NoPricePoint"
-		el.innerHTML = point.mapPlace.value
+		// Wrapper for centering point
+		const marker = document.createElement("div")
+		marker.className = "no-price-point-marker"
+		
+		const wrapper = document.createElement("div")
+		wrapper.className = "no-price-point-wrapper"
+		marker.appendChild(wrapper)
+		
+		// Price point for :after triangle
+		const noPricePoint = document.createElement("div")
+		noPricePoint.className = "no-price-point"
+		wrapper.appendChild(noPricePoint)
+		
+		// Actual content for being over the :after triangle in Price point
+		const element = document.createElement("div")
+		element.className = "no-price-point-content"
+		noPricePoint.appendChild(element)
+		
+		const icon = document.createElement("i")
+		icon.className = "no-price-point-icon "
+		icon.className += "spIcon ic_location_city"
+		
+		element.appendChild(icon)
+		
+		// Back content for being over the :after triangle in Price point
+		const elementBack = document.createElement("div")
+		elementBack.className = "no-price-point-content-back"
+		noPricePoint.appendChild(elementBack)
+		
+		const title = document.createElement("div")
+		title.className = "no-price-point-title"
+		title.innerHTML = "<span>" + point.mapPlace.value + "</span>"
+		elementBack.appendChild(title)
+		super(marker, point)
 		
 		this.setLngLat([point.mapPlace.lng, point.mapPlace.lat])
 		this.addTo(map)
--- src/view/points/PricePoint.js
+++ src/view/points/PricePoint.js
@@ -2,10 +2,51 @@ import Point from "./Point"
 
 export default class PricePoint extends Point {
 	constructor(map, point) {
-		const el = document.createElement("div")
-		super(el, point)
-		el.className += " PricePoint"
-		el.innerHTML = point.mapPlace.value
+		const marker = document.createElement("div")
+		marker.className = "price-point-marker"
+		
+		const wrapper = document.createElement("div")
+		wrapper.className = "price-point-wrapper"
+		// TODO: Highlighted
+		if (point.type === "HIGHLIGHTED") {
+			wrapper.className += " highlighted"
+		}
+		marker.appendChild(wrapper)
+		
+		// Price point for :after triangle
+		const pricePoint = document.createElement("div")
+		pricePoint.className = "price-point"
+		wrapper.appendChild(pricePoint)
+		
+		// Actual content for being over the :after triangle in Price point
+		const element = document.createElement("div")
+		element.className = "price-point-content"
+		pricePoint.appendChild(element)
+		// Back content for being over the :after triangle in Price point
+		const elementBack = document.createElement("div")
+		elementBack.className = "price-point-content-back"
+		pricePoint.appendChild(elementBack)
+		
+		const title = document.createElement("div")
+		title.className = "price-point-title"
+		title.innerHTML = "<span>" + point.mapPlace.value + "</span>"
+		elementBack.appendChild(title)
+		super(marker, point)
+		
+		if (point.mapPlace.price) {
+			let priceFront = document.createElement("div")
+			priceFront.className = "price-point-price"
+			priceFront.innerHTML = "<span>" + point.mapPlace.price + "</span>"
+			
+			element.appendChild(priceFront)
+			
+			let priceBack = document.createElement("div")
+			priceBack.className = "price-point-price"
+			priceBack.innerHTML = "<span>" + point.mapPlace.price + "</span>"
+			
+			elementBack.appendChild(priceBack)
+		}
+		window.point = point
 		
 		this.setLngLat([point.mapPlace.lng, point.mapPlace.lat])
 		this.addTo(map)
